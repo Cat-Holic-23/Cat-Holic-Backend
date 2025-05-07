c812db216fc25bc6868c9e9247577e8d531dc2ce
taehoondev030 <taehoondev030@gmail.com>
2025-05-07 21:17:29 +0900

    OAuth2UserService 완료

--- src/main/java/moodi/backend/service/CustomOAuth2UserService.java
+++ src/main/java/moodi/backend/service/CustomOAuth2UserService.java
@@ -1,5 +1,8 @@
 package moodi.backend.service;
 
+import moodi.backend.dto.GoogleResponse;
+import moodi.backend.dto.OAuth2Response;
+import moodi.backend.dto.UserDTO;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
@@ -19,10 +22,22 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
         String registrationId = userRequest.getClientRegistration().getRegistrationId();
         OAuth2Response oAuth2Response = null;
 
-        if(registrationId.equals("google")) {
+        if (registrationId.equals("google")) {
             oAuth2Response = new GoogleResponse(oAuth2User.getAttributes());
         } else {
             return null;
         }
+
+        String username = oAuth2Response.getProvier() + " " + oAuthResponse.getProverId();
+
+        UserDTO userDTO = new UserDTO();
+        userDTO.setUsername(username);
+        userDTO.setName(oAuth2Response.getName());
+        userDTO.setRole("ROLE_USER");
+
+        return new CustomOAuth2User(userDTO);
     }
+
+
+
 }
