500739896b6a2ec7770824bc079ad1ff361977c0
taehoondev030 <taehoondev030@gmail.com>
2025-05-10 17:28:12 +0900

    코드 정리

--- src/main/java/moodi/backend/user/controller/UserController.java
+++ src/main/java/moodi/backend/user/controller/UserController.java
@@ -2,13 +2,10 @@ package moodi.backend.user.controller;
 
 import lombok.RequiredArgsConstructor;
 import moodi.backend.user.CustomUserDetails;
-import moodi.backend.user.domain.User;
 import moodi.backend.user.dto.UserResponse;
 import moodi.backend.user.service.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +19,7 @@ public class UserController {
     public ResponseEntity<UserResponse> getUserInfo(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
         String username = customUserDetails.getUsername();  // 인증된 사용자 정보
         UserResponse userResponse = userService.getUserInfo(username);
-        
+
         return ResponseEntity.ok(userResponse);
     }
 
